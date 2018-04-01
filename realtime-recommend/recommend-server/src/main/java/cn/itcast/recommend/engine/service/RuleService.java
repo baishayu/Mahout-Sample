f633458e3c97823cfd36b29b7748ceda57567bb7
zhangsan <zhangsan@163.com>
2018-04-01 22:38:08 +0800

    mahout recommend init

---
+++ realtime-recommend/recommend-server/src/main/java/cn/itcast/recommend/engine/service/RuleService.java
@@ -0,0 +1,11 @@
+package cn.itcast.recommend.engine.service;
+
+import cn.itcast.recommend.engine.domain.Template;
+
+public interface RuleService {
+
+    Template getTemplateByAdId(String adId);
+
+    boolean isExist(String adId);
+
+}
